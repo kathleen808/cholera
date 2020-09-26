0cc52ff9a82430e353a30b39e92806a18a34f271
Kathleen <kdy3@hawaii.edu>
2020-09-26 02:10:29 -1000

    Created header and footer templates; created Data, Tools, and Contact Pages

---
+++ footer.js
@@ -0,0 +1,27 @@
+function buildFooter() {
+    let footer = document.getElementById('footer');
+    let element = document.createElement('div');
+    element.innerHTML +=
+    '<div id="footer" class="footer-background"> \
+        <div class="ui two column grid container"> \
+            <div class="column"> \
+                <h3>QUICK LINKS</h3> \
+                <hr/> \
+                <div class="ui inverted link list"> \
+                    <a href="index.html" class="item">Home</a> \
+                    <a href="data.html" class="item">Data</a> \
+                    <a href="tools.html" class="item">Tools & Libraries</a> \
+                </div> \
+            </div> \
+            <div class="column"> \
+                <h3>CONNECT</h3> \
+                <hr/> \
+                <div class="ui inverted link list"> \
+                    <a href="contact.html" class="item">Contact</a> \
+                    <a href="https://github.com/kathleen808/cholera" class="item">Github</a> \
+                </div> \
+            </div> \
+        </div> \
+    </div>'
+    footer.appendChild(element);
+}
---
+++ header.js
@@ -0,0 +1,15 @@
+function buildHeader() {
+    let header = document.getElementById('header');
+    let element = document.createElement('div');
+    element.innerHTML +=
+    '<div id="navBar" class="ui left inverted fluid menu"> \
+        <a href="https://kathleen808.github.io/cholera/" class="my-menu-item ui simple item">HOME</a> \
+        <a href="data.html" class="my-menu-item ui simple item">DATA</a> \
+        <a href="tools.html" class="my-menu-item ui simple item">TOOLS & LIBRARIES</a> \
+        <a href="contact.html" class="my-menu-item ui simple item">CONTACT</a> \
+        <a href="https://github.com/kathleen808/cholera" class="my-menu-item ui simple right item"> \
+            <img src="./images/GitHub-Mark-Light-120px-plus.png" style="height: 30px; width: 30px;"> \
+        </a> \
+    </div>'
+    header.appendChild(element);
+}
